9f6c1e6c8eefb3f0ffc554d0a9f42298c64e03bb
Rasiga1 <geoujif@gmail.com>
2025-05-09 04:32:52 +0300

    Исправлен импорт функций для тестов

--- tests/test_Rasiga.py
+++ tests/test_Rasiga.py
@@ -1,16 +1,14 @@
 from src.calc import calculate
 
-def test_addition():
-    assert add(1, 2) == 3
-    assert add(-1, -1) == -2
-    assert add(0, 0) == 0
 
-def test_subtraction():
-    assert subtract(5, 3) == 2
-    assert subtract(0, 4) == -4
-    assert subtract(-3, -2) == -1
-
-def test_multiplication():
-    assert multiply(2, 3) == 6
-    assert multiply(-1, 5) == -5
-    assert multiply(0, 100) == 0
+def test_calc_division(): # тестирование деления
+    assert calculate(10, 2, '/') == 5
+    
+def test_calc_subtraction(): # тестирование вычитания
+    assert calculate(10, 2, '-') == 8
+#
+def test_calc_multiplication(): # тестирование умножение
+    assert calculate(10, 2, '*') == 20
+#
+def test_calc_addition(): # тестирование cложения
+    assert calculate(10, 2, '+') == 12
